ddd7264e507d7f69258b1a5f5171838bcc3c32de
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-11-18 16:52:55 +0700

    Create variable noMhs,name,next,prev

--- DoubleLinked_list/Program.cs
+++ DoubleLinked_list/Program.cs
@@ -4,15 +4,25 @@ namespace DoubleLinked_list
 {
     class Node
     {
-       
+        //Node class represent the node of doubly linked list
+        //It consist of the information part and links to
+        //Its seccemdimh amd proceeding
+        //in terms of next and previous
+
+        public int noMhs;
+        public string name;
+        //point to the succending node
+        public Node next;
+        //point to preccedning node
+        public Node prev;
 
     }
     class DoubleLinkedList
     {
-        
+ 
     }
     class Program
     {
-       
+    
     }
 }
