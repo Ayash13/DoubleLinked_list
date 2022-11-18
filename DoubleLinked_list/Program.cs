48fcd8ba16f80de740472e973f27aa37ddcda751
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-11-18 17:51:29 +0700

    Create function to unserted node between two node

--- DoubleLinked_list/Program.cs
+++ DoubleLinked_list/Program.cs
@@ -50,7 +50,19 @@ namespace DoubleLinked_list
                 START = newNode;
                 return;
             }
-        }
+            //if the node is to be inserted at between two Node
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("Duplicate roll number is not allowed");
+                    return;
+                }
+            }
+            
     }
     class Program
     {
